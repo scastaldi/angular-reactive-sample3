5e051faf4948154cdc04fd5af3e40c9b996a21fa
scastaldi <scastaldi@users.noreply.github.com>
2022-10-04 21:15:13 -0400

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  name = 'TampaCC 2022';
+  name = 'Tampacc 2022 - Sample3';
 }
